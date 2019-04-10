4c1d74f7ab4c45bcca9f6784e303401c1afa41d5
sahil sahil <d17124269@mydit.ie>
2019-04-10 17:13:56 +0100

    made constructor of resistor

--- src/ie/tudublin/Resistor.java
+++ src/ie/tudublin/Resistor.java
@@ -17,4 +17,71 @@ public class Resistor
         this.tens = tens;
         this.hundreds = hundreds;
     }
+
+    public Resistor(TableRow tr)
+        {
+        this(tr.getFloat("value"),tr.getFloat("ones"),tr.getFloat("tens"),tr.getString("hundreds")); 
+        }
+
+    /**
+     * @return the value
+     */
+    public float getValue() {
+        return value;
+    }
+
+    /**
+     * @param value the value to set
+     */
+    public void setValue(float value) {
+        this.value = value;
+    }
+
+    /**
+     * @return the ones
+     */
+    public float getOnes() {
+        return ones;
+    }
+
+    /**
+     * @param ones the ones to set
+     */
+    public void setOnes(float ones) {
+        this.ones = ones;
+    }
+
+    /**
+     * @return the tens
+     */
+    public float getTens() {
+        return tens;
+    }
+
+    /**
+     * @param tens the tens to set
+     */
+    public void setTens(float tens) {
+        this.tens = tens;
+    }
+
+    /**
+     * @return the hundreds
+     */
+    public String getHundreds() {
+        return hundreds;
+    }
+
+    /**
+     * @param hundreds the hundreds to set
+     */
+    public void setHundreds(String hundreds) {
+        this.hundreds = hundreds;
+    }
+
+    public String toString()
+    {
+        return value +  "\t" + ones + "\t" + tens + "\t" + hundreds;
+    }
+   
 }
