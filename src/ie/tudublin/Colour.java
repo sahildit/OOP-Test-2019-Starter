8d0e4ad7a462bb84fe7768f2c2e7a7dbf54f1fc9
sahil sahil <d17124269@mydit.ie>
2019-04-10 16:26:46 +0100

    made a class called Colour

---
+++ src/ie/tudublin/Colour.java
@@ -0,0 +1,23 @@
+package ie.tudublin;
+
+import javax.swing.table.TableColumn;
+
+import processing.data.*;
+public class Colour
+{
+    public float r;
+    public float g;
+    public float b;
+    private String colour;
+
+    public Colour(float r,float g, float b,String colour)
+    {
+        this.r = r;
+        this.g  =g;
+        this.b = b;
+        this.colour = colour;
+        
+    }
+
+   
+}
